c0963d6d826cf4ec23dd81dea53db623136be56d
Tamires <tp.tamirespereira@gmail.com>
2023-09-28 14:58:55 -0300

    feat/multiplicacao

---
+++ multiplicacao.py
@@ -0,0 +1,13 @@
+
+
+def multiplicacao(n1: int, n2: int) -> int:
+    """Realiza a multiplicação entre dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        O produto entre os dois números.
+    """
+    return n1 * n2
