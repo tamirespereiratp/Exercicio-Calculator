cea5fc438027ade7e82477b6fa04dfe4aac1a246
Tamires <tp.tamirespereira@gmail.com>
2023-09-28 14:56:08 -0300

    develop

---
+++ calculator.py
@@ -0,0 +1,50 @@
+def adicao(n1: int, n2: int) -> int:
+    """Soma dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        A soma dos dois números.
+    """
+    return n1 + n2
+
+
+def multiplicacao(n1: int, n2: int) -> int:
+    """Realiza a multiplicação entre dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        O produto entre os dois números.
+    """
+    return n1 * n2
+
+
+def subtracao(n1: int, n2: int) -> int:
+    """Realiza a subtração entre dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        A subtração dos dois números.
+    """
+    return n1 - n2
+
+
+def divisao(n1: int, n2: int) -> int:
+    """Realiza a divisão entre dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        O quociente dos dois números.
+    """
+    return n1 / n2
