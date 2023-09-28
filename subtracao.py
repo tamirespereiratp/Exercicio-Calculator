5bd23663f0a1378799ce1e48ce7209cf0a493036
Tamires <tp.tamirespereira@gmail.com>
2023-09-28 14:58:18 -0300

    feat/subtracao

---
+++ subtracao.py
@@ -0,0 +1,13 @@
+
+
+def subtracao(n1: int, n2: int) -> int:
+    """Realiza a subtração entre dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        A subtração dos dois números.
+    """
+    return n1 - n2
