91ec9a0fb49be8a8360af46ca8c0a10e9b0c6e96
Tamires <tp.tamirespereira@gmail.com>
2023-09-28 14:56:31 -0300

    feat/adicao

---
+++ adicao.py
@@ -0,0 +1,13 @@
+
+
+def adicao(n1: int, n2: int) -> int:
+    """Soma dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        A soma dos dois números.
+    """
+    return n1 + n2
