d53d3c3db8121e4939ddf5febdb972f7a6837dc1
Tamires <tp.tamirespereira@gmail.com>
2023-09-28 14:58:33 -0300

    feat/divisao

---
+++ divisao.py
@@ -0,0 +1,13 @@
+
+
+def divisao(n1: int, n2: int) -> int:
+    """Realiza a divisão entre dois números.
+
+    Args:
+        n1: O primeiro número.
+        n2: O segundo número.
+
+    Returns:
+        O quociente dos dois números.
+    """
+    return n1 / n2
